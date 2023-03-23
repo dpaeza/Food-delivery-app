c976fda23909b0733a718d3370c24487e69e1019
dpaeza <danipaez1098@gmail.com>
2023-03-22 19:24:37 -0500

    add Routes and components

--- src/App.js
+++ src/App.js
@@ -1,24 +1,33 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Splash from "./components/splash/Splash.jsx";
+import Slide from "./components/slide/Slide.jsx";
+import SignIn from "./components/singIn/SignIn.jsx";
+import Verification from "./components/verification/Verification.jsx";
+import CreateAcount from "./components/createAcount/CreateAcount.jsx";
+import Location from "./components/location/Location";
+import Home from "./components/home/Home";
+import Search from "./components/search/Search";
+import Orders from "./components/orders/Orders";
+import Profile from "./components/profile/Profile";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Splash />} />
+        <Route path="/slide" element={<Slide />} />
+        <Route path="/sign_in" element={<SignIn />} />
+        <Route path="/verification" element={<Verification/>} />
+        <Route path="/create_acount" element={<CreateAcount />} />
+        <Route path="/location" element={<Location />} />
+        <Route path="/home" element={<Home />} />
+        <Route path="/search" element={<Search />} />
+        <Route path="/orders" element={<Orders />} />
+        <Route path="/profile" element={<Profile />} />
+      
+      </Routes>
+    </BrowserRouter>
   );
 }
 
---
+++ src/components/verification/Verification.jsx
@@ -0,0 +1,10 @@
+import React from "react";
+
+const Verification = () => {
+
+    return (
+        <></>
+    )
+}
+
+export default Verification
