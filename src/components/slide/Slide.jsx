71f6f148b2a1763ba13f9b696ffdc93684beb88b
dpaeza <danipaez1098@gmail.com>
2023-03-24 11:32:21 -0500

    add layout splash and slide

--- src/components/slide/Slide.jsx
+++ src/components/slide/Slide.jsx
@@ -1,9 +1,36 @@
 import React from "react";
+import { Carousel } from "react-responsive-carousel";
+import "react-responsive-carousel/lib/styles/carousel.css";
+import slide_1 from "../../assets/slide_1.svg";
+import slide_2 from "../../assets/slide_2.svg";
+import slide_3 from "../../assets/slide_3.svg";
 
 const Slide = () => {
 
     return (
-        <></>
+        <body className="slide">
+            <Carousel showArrows={false} showThumbs={false} showStatus={false}>
+                <div>
+                    <figure>
+                        <img src={slide_1} alt="img" />
+                    </figure>
+                    <p>Choose what to eat choosing from a variety of restaurants from the list</p>
+                </div>
+                <div>
+                    <figure>
+                        <img src={slide_2} alt="img" />
+                    </figure>
+                    <p>Choose where you want to deliver by indicating on the map</p>
+                </div>
+                <div>
+                    <figure>
+                        <img src={slide_3} alt="img" />
+                    </figure>
+                    <p>We will deliver as soon as possible</p>
+                </div>
+            </Carousel>
+            <button>Next</button>
+        </body>
     )
 }
 
--- src/components/splash/Splash.jsx
+++ src/components/splash/Splash.jsx
@@ -1,9 +1,14 @@
 import React from "react";
+import maina_icon from "../../assets/main_icon.svg"
 
 const Splash = () => {
 
     return (
-        <></>
+        <section className="splash">
+            <figure>
+                <img src={maina_icon} alt="logo app" />
+            </figure>
+        </section>
     )
 }
 
