bba803d9d0f89bfb3701e33b41786d984d2f1295
dpaeza <danipaez1098@gmail.com>
2023-03-24 14:49:22 -0500

    add setTimeout on splash page to router to slide page

--- src/components/splash/Splash.jsx
+++ src/components/splash/Splash.jsx
@@ -1,8 +1,15 @@
 import React from "react";
 import maina_icon from "../../assets/main_icon.svg";
 import 'animate.css';
+import { useNavigate } from "react-router-dom";
 
 const Splash = () => {
+
+    const navigate = useNavigate();
+
+    setTimeout(() => {
+        navigate("/slide")
+    }, 3000);
 
     return (
         <section className="splash">
