5afba68dad5aaa5278724c97fb109cf465c33244
dpaeza <danipaez1098@gmail.com>
2023-03-25 17:10:55 -0500

    add create account layout

--- src/components/createAccount/CreateAccount.jsx
+++ src/components/createAccount/CreateAccount.jsx
@@ -3,8 +3,29 @@ import React from "react";
 const CreateAccount = () => {
 
     return (
-        <></>
-    )
+        <section className="createAccount">
+            <section className="createAccount__modal">
+                <h1>Create account</h1>
+                <form>
+                    <section>
+                        <div>
+                            <label>NAME</label>
+                            <input type="text" />
+                        </div>
+                        <div>
+                            <label>EMAIL</label>
+                            <input type="email" />
+                        </div>
+                        <div>
+                            <label>PASSWORD</label>
+                            <input type="password" />
+                        </div>
+                    </section>
+                    <button>Sing in</button>
+                </form>
+            </section>
+        </section>
+    );
 }
 
 export default CreateAccount
