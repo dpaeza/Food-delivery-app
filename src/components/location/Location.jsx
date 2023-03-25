af4fcb4811bac7e7dadeb4176084705f04d03025
dpaeza <danipaez1098@gmail.com>
2023-03-25 17:31:32 -0500

    add location layout

--- src/components/location/Location.jsx
+++ src/components/location/Location.jsx
@@ -1,10 +1,26 @@
 import React from "react";
+import map from "../../assets/map.png"
 
 const Location = () => {
 
     return (
-        <></>
-    )
+        <section className="location">
+            <section className="location__modal">
+                <div>
+                    <p className="location__pinitial">
+                        Allow access to geo data on the device mark your address
+                        on the map
+                    </p>
+                    <figure>
+                        <img src={map} alt="map image" />
+                    </figure>
+                    <p className="location__whereIam">Where I am ?</p>
+                    <input type="text" />
+                </div>
+                <button>Confirm</button>
+            </section>
+        </section>
+    );
 }
 
 export default Location
