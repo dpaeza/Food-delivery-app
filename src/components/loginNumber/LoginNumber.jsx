423c25c4cbaec6e7dab93f54bc8e38ad4b905f55
dpaeza <danipaez1098@gmail.com>
2023-04-01 22:58:55 -0500

    initialice login with phone number

--- src/components/loginNumber/LoginNumber.jsx
+++ src/components/loginNumber/LoginNumber.jsx
@@ -3,11 +3,38 @@ import logo from "../../assets/main_icon.svg";
 import phoneGrey from "../../assets/phone_grey.svg"
 import { useNavigate } from "react-router-dom";
 import { useSelector } from "react-redux";
+import { yupResolver } from "@hookform/resolvers/yup";
+import * as yup from "yup";
+import { useForm } from "react-hook-form";
+import { RecaptchaVerifier, signInWithPhoneNumber } from "firebase/auth";
+import { auth } from "../../firebase/firebaseConfig";
+
+//const que valida que el numero telefonico sea numero y que tenga 10 digitos
+const numberRegex = /^[0-9]{10}$/;
+
+const schema = yup
+    .object({
+        phone: yup
+            .string()
+            .required("Phone number is required")
+            .matches(numberRegex, "The phone number must be 10 digits"),
+    })
+    .required();
 
 const LoginNumber = () => {
 
     const navigate = useNavigate();
     const user = useSelector((state) => state.user);
+        const {
+            register,
+            handleSubmit,
+            setValue,
+            formState: { errors },
+    } = useForm({ resolver: yupResolver(schema) });
+    
+    const submitNumber = (data) => { 
+        console.log(data)
+    }
 
     //Use effect para redirija al usuario a home si ya está loggueado
     useEffect(() => {
@@ -15,6 +42,26 @@ const LoginNumber = () => {
             navigate("/home");
         }
     }, [user.isLogged]);
+
+    const generateRecaptcha = () => {
+        try {
+            window.recaptchaVerifier = new RecaptchaVerifier(
+                "recaptcha-container",
+                {
+                    size: "invisible",
+                    callback: (response) => { },
+                },
+                auth
+            );
+        } catch (error) {
+            console.log(error)
+        }
+        
+    };
+
+    const sendSMS = (phoneNumber) => {
+        signInWithPhoneNumber()
+    }
 
     return (
         <section className="login">
@@ -26,16 +73,26 @@ const LoginNumber = () => {
                 Login or create an account with your phone number to start
                 ordering
             </p>
-            <form className="loginNumber__form">
-                <div>
+            <form
+                className="loginNumber__form"
+                onSubmit={handleSubmit(submitNumber)}
+            >
+                <div className="loginNumber__form__div">
                     <figure>
                         <img src={phoneGrey} alt="phone icon" />
                     </figure>
                     <p>+</p>
-                    <input type="number" />
+                    <input type="number" {...register("phone")} />
+                    {errors.phone ? (
+                        <span className="createAccount__error">
+                            {errors.phone.message}
+                        </span>
+                    ) : (
+                        <></>
+                    )}
                 </div>
-
-                <button>Login</button>
+                <div id="recaptcha-container"></div>
+                <button type="submit">Login</button>
             </form>
         </section>
     );
